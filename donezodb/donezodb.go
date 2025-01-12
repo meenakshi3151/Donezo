d31c6fc4a41cfa805c690c5d8e8237ba87d7e843
Meenakshi Gupta <guptameenakshi957@gmail.com>
2025-01-12 10:52:14 +0530

    added login user functionality

--- donezodb/donezodb.go
+++ donezodb/donezodb.go
@@ -7,28 +7,56 @@ import (
 )
 
 func Connect() (*sql.DB, error) {
-	const donezodb string = "donezo.db"
+	const donezodb string = "doneZo.db"
 	db, err := sql.Open("sqlite3", donezodb)
 	if err != nil {
 		fmt.Println("Error opening the database:", err)
 		return nil, err
 	}
-
 	if err := db.Ping(); err != nil {
 		fmt.Println("Error connecting to the database:", err)
 		return nil, err
 	}
-
 	fmt.Println("DB connection successful")
 	return db, nil
 }
 
-func CheckEmail (db *sql.DB, email string) bool {
+func CheckEmail(db *sql.DB, email string) bool {
 	var count int
-	err := db.QueryRow("SELECT COUNT(*) FROM users WHERE email = ?", email).Scan(&count)
+	// this value 2  will be assigned to the count variable via the Scan method.
+	err := db.QueryRow("SELECT COUNT(*) FROM user WHERE email = ?", email).Scan(&count)
 	if err != nil {
+		fmt.Println(count)
 		fmt.Println("Error checking for duplicate email:", err)
 		return false
 	}
-	return count == 0
+	if count > 0 {
+	return false
+	}
+	return true
+}
+
+func InsertUser(db *sql.DB, firstName string, lastName string, email string, password string) {
+	_,err := db.Exec("INSERT INTO user (First_name, Last_name, Email, Password) VALUES (?, ?, ?, ?)", firstName, lastName, email, password)
+	if err != nil {
+		fmt.Println("Error inserting user:", err)
+	} else {
+	fmt.Println("User inserted successfully")
+	}
+}
+
+func LoginUser(db *sql.DB, email string, password string) bool {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM user WHERE email = ? AND password = ?", email, password).Scan(&count)
+	if err != nil {
+		fmt.Println("Error checking for user:", err)
+		return false
+	}
+	if count > 0 {
+		fmt.Println("User found")
+	} else {
+		fmt.Println("User not found. Provide correct email and password")
+		return false
+	}
+	return true
 }
--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func greetUsers() {
 }
 
 func verifyEmail(email string) bool {
-	var specialChar = []string{"!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", ",", ".", "?", "/", "|", "`", "~"}
+	var specialChar = []string{"!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "}", "[", "]", ":", ";", "'", "<", ">", ",",  "?", "/", "|", "`", "~"}
 	for _, char := range specialChar {
 		if strings.Contains(email, char) {
 			return false
@@ -23,11 +23,11 @@ func verifyEmail(email string) bool {
 	return strings.Contains(email, "@gmail.com")
 }
 
-func verification(firstName string, lastName string, email string, db *sql.DB) {
-	if len(firstName) > 2 && len(lastName) > 2 && verifyEmail(email) {
+func verification(firstName string, lastName string, email string, password string, db *sql.DB) {
+	if len(firstName) > 2 && len(lastName) > 2 && verifyEmail(email) && len(password) >= 8 {
 		fmt.Println("You are verified")
 	} else {
-		fmt.Println("Please enter a valid name with at least 2 characters.")
+		fmt.Println("Please enter a valid name and email")
 		countOfDetails++
 		if countOfDetails == 2 {
 			fmt.Println("You have reached the limit of entering the details")
@@ -37,9 +37,9 @@ func verification(firstName string, lastName string, email string, db *sql.DB) {
 	}
 }
 
-func checkDuplicateEmail(db *sql.DB,email string) {
+func checkDuplicateEmail(db *sql.DB,email string, firstName string, lastName string, password string) {
    if(donezodb.CheckEmail(db, email)) {
-	   
+	   donezodb.InsertUser(db, firstName, lastName, email, password)
    } else {
 	   fmt.Println("Email is duplicate. Use another email")
 	   getUserDetails(db)
@@ -50,17 +50,42 @@ func getUserDetails(db *sql.DB) {
 	var firstName string
 	var lastName string
 	var email string
+	var password string
 	fmt.Print("Enter your first name: ")
 	fmt.Scan(&firstName)
 	fmt.Print("Enter your last name: ")
 	fmt.Scan(&lastName)
 	fmt.Print("Enter your email address: ")
 	fmt.Scan(&email)
+	fmt.Print(("Enter your password: "))
+	fmt.Scan(&password)
 	fmt.Println("First Name: ", firstName)
 	fmt.Println("Last Name: ", lastName)
 	fmt.Println("Email: ", email)
-	verification(firstName, lastName, email, db)
-	checkDuplicateEmail(db, email)
+	verification(firstName, lastName, email, password, db)
+	checkDuplicateEmail(db, email, firstName, lastName, password)
+}
+
+func askForExistingUser(db *sql.DB) bool {
+	var choice string
+	fmt.Print("Are you an existing user? (y/n): ")
+	fmt.Scan(&choice)
+	return choice == "y"
+}
+
+func loginUser(db *sql.DB) {
+	var email string
+	var password string
+	fmt.Print("Enter your email address: ")
+	fmt.Scan(&email)
+	fmt.Print("Enter your password: ")
+	fmt.Scan(&password)
+	if donezodb.LoginUser(db, email, password) {
+		fmt.Println("Login successful")
+	} else {
+		fmt.Println("Login failed")
+		loginUser(db)
+	}
 }
 
 func main() {
@@ -71,5 +96,11 @@ func main() {
 	}
 	defer db.Close()
 	greetUsers()
-	getUserDetails(db) 
+	if askForExistingUser(db) {
+		fmt.Println("Welcome back")
+		loginUser(db)
+	} else {
+		getUserDetails(db)
+	}
+    
 }
